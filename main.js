244109c0fd5d1d5395fd0a2e932b25b011cae066
Bzparkes <bzparkes@yahoo.com.au>
2022-05-04 19:55:58 +1000

    Setting up the initial functions and psuedo-coding the JS

---
+++ main.js
@@ -0,0 +1,52 @@
+// const num1 = 1;
+// const num2 = 2;
+// const num3 = 3;
+// const num4 = 4;
+// const num5 = 5;
+// const num6 = 6;
+// const num7 = 7;
+// const num8 = 8;
+// const num9 = 9;
+
+//console.log(num2 * num9)
+
+const display = document.querySelector('section')
+
+function enterNumber(num) {
+     return num;
+    //Takes the button pressed and enters that value
+}
+
+function addition() {
+    return;
+    //Adds new number to previous number
+}
+
+function subtraction() {
+    return;
+    //Subtracts the new number from the previous number
+}
+
+function multiply() {
+    return num1 * num2;
+    //Multiplies the previous number by the new number
+}
+
+function divide() {
+    return num1 / num2;
+    //Divides the previous number by the new number
+}
+
+function equals() {
+    //Sums the numbers by the euqation used    
+}
+
+function clearEntry() {
+    //Clears the last number/operation entered
+}
+
+function clearAll() {
+    //Clears all entered data and returns to original state
+}
+
+console.log(addition(1,2,3))
